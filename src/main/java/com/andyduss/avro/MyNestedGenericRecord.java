769b7b91b0df94db7bd348a022509e85a5fd3797
Andy D <mindovermiles262@gmail>
2020-05-31 15:54:09 -0700

    working with generic and nested objects. TODO: nested unions with NULL

--- src/main/java/com/andyduss/avro/MyNestedGenericRecord.java
+++ src/main/java/com/andyduss/avro/MyNestedGenericRecord.java
@@ -6,15 +6,13 @@ import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.generic.*;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
-import sun.net.www.content.text.Generic;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MyGenericRecord {
+public class MyNestedGenericRecord {
     public static void main(String[] args) throws IOException {
         //
         // Load Schema
@@ -22,7 +20,7 @@ public class MyGenericRecord {
 
         Schema.Parser parser = new Schema.Parser();
         // File is relative to project base directory
-        File schemaFile = new File("src/main/resources/schema.avsc");
+        File schemaFile = new File("src/main/resources/nested_schema.avsc");
         Schema schema = parser.parse(schemaFile);
 
         //
@@ -30,37 +28,49 @@ public class MyGenericRecord {
         //
 
         GenericRecordBuilder grBuilder = new GenericRecordBuilder(schema);
-        grBuilder.set("name", "Klaira");
-        grBuilder.set("age", 29);
-        grBuilder.set("favorite_color", "Green");
+        grBuilder.set("name", "Andy");
+        grBuilder.set("age", 30);
+        grBuilder.set("favorite_color", "Blue");
 
+        // Build nested record object (manager)
+        Schema managerSchema = schema.getField("manager").schema();
+        GenericRecordBuilder mgrBuilder = new GenericRecordBuilder(managerSchema);
+        mgrBuilder.set("id", 2);
+        mgrBuilder.set("code", "base64encoded==");
+        GenericData.Record manager = mgrBuilder.build();
+
+        // Attach manager to base Generic Record
+        grBuilder.set("manager", manager);
+
+        // Build User
         GenericData.Record user = grBuilder.build();
         System.out.println("Built user: " + user);
 
         //
         // Write User to File
         //
-        
+
         DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(schema);
         DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter);
-        
+
         try {
             Schema userSchema = user.getSchema();
-            File outFile = new File("src/main/resources/user.avro");
+            File outFile = new File("src/main/resources/nested_user.avro");
             dataFileWriter.create(userSchema, outFile);
             dataFileWriter.append(user);
             dataFileWriter.close();
             System.out.println("[+] Wrote user to " + outFile.getPath());
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("[!] Error Writing to File");
             e.printStackTrace();
+            System.exit(1);
         }
 
         //
         // Read User from File
         //
 
-        File userLoadFile = new File("src/main/resources/user.avro");
+        File userLoadFile = new File("src/main/resources/nested_user.avro");
         DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
         GenericRecord userRead;
         DataFileReader<GenericRecord> userFileReader = new DataFileReader<>(userLoadFile, datumReader);
